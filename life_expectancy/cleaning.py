e6e276bbd3e562a0447a20ddd33cf0a269cbd0c9
lpedrossantos <lpedrossantos@gmail.com>
2023-12-12 12:54:36 +0000

    fix lint warnings

--- life_expectancy/cleaning.py
+++ life_expectancy/cleaning.py
@@ -7,8 +7,10 @@ args, _ = parser.parse_known_args()
 
 def clean_data(region=args.region):
     """
-    This function loads and cleans the data from eu_life_expectancy_raw dataset and saves the cleaned dataset on the data folder.
-    By default filters for the PT region but it can also be filter for another region by passing the command line argument --region
+    This function loads and cleans the data from eu_life_expectancy_raw dataset
+    and saves the cleaned dataset on the data folder.
+    By default filters for the PT region but it can also be filter
+    for another region by passing the command line argument --region
     
     Args:
         region: Type of Country to filter dataset
@@ -17,9 +19,9 @@ def clean_data(region=args.region):
         None
     """
     data = pd.read_csv(
-        './life_expectancy/data/eu_life_expectancy_raw.tsv', 
-        sep= r'\,|\t', 
-        header=0, 
+        './life_expectancy/data/eu_life_expectancy_raw.tsv',
+        sep= r'\,|\t',
+        header=0,
         engine='python'
     )
 
